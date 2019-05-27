3416728fefa9b1c02108d50cdc409168717750d8
gallottino <d.gallotti@outlook.it>
2019-05-27 20:00:16 +0200

    Added new branch for developing

---
+++ Agent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dgall on 27/05/2019.
+//
+
+#include "Agent.h"
---
+++ Agent.h
@@ -0,0 +1,14 @@
+//
+// Created by dgall on 27/05/2019.
+//
+
+#ifndef TICTACTOE_AGENT_H
+#define TICTACTOE_AGENT_H
+
+
+class Agent {
+
+};
+
+
+#endif //TICTACTOE_AGENT_H
